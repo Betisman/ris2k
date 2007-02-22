bb7d1ba92a59b6194cdfb6c71ef58013981397de
msfer2000 <msfer2000@21717f70-f929-0410-a4ed-59d6e259485b>
2007-02-22 17:16:10 +0000

    Estos son los formularios

---
+++ trunk/ris2k/web/form.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>RIS2K REGISTER PAGE</title>
+</head>
+<body>
+<form action="welcome.php" method="post">
+First Name:<input type="text" size="12" maxlength="12" name="name"><br />
+Last Name:<input type="text" size="12" maxlength="36" name="lastname"><br />
+Email:<input type="text" size="20" maxlength="36" name="email"><br />
+<input type="submit" />
+</form>
+</body>
+</html>
