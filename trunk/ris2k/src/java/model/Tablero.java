6fe9d8460a4ca3a6c95a25743bb4647ece3dec0b
betisman <betisman@21717f70-f929-0410-a4ed-59d6e259485b>
2007-03-09 15:06:13 +0000

    tablero.java con un metodo nuevo

--- trunk/ris2k/src/java/model/Tablero.java
+++ trunk/ris2k/src/java/model/Tablero.java
@@ -9,7 +9,19 @@
 
 package model;
 
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Collection;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 /**
  *
@@ -42,5 +54,46 @@ public class Tablero {
         if (mapa == null){
             return true;
         }else return false;
+    }
+    
+    public void cambiarLinksTerritorios(String newLink){
+        
+        try{
+            File f = new File("web/test/dibujo.xml");
+            if (f.exists()){
+                System.out.println("existe el fichero del mapa... " + f.getPath());
+            }else{
+                System.out.println("ouch!! no existe el fichero del mapa... " + f.getPath());
+            }
+
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder(); 
+            Document document = builder.parse(new File("web/test/dibujo.xml"));
+            
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            String expression = "//a";
+            NodeList nodos = (NodeList)xpath.evaluate(expression, document, XPathConstants.NODE);
+            for(int i=0; i<nodos.getLength(); i++){
+                Node n = nodos.item(i);
+                String idTerritorio = n.getAttributes().getNamedItem("id").getTextContent();
+System.out.println(idTerritorio);
+                idTerritorio = idTerritorio.replace("link", "");
+System.out.println(idTerritorio);
+                String link = newLink + "?territorio=" + idTerritorio;
+System.out.println(link);
+                n.getAttributes().getNamedItem("xlink:href").setTextContent(link);
+System.out.println(idTerritorio);
+System.out.println(n.getAttributes().getNamedItem("xlink:href").getTextContent());
+            }
+           
+            OutputFormat format = new OutputFormat(document);
+            FileWriter fileout = new FileWriter("web/test/dibujo.xml");
+            XMLSerializer serial = new XMLSerializer(fileout, format);
+            serial.asDOMSerializer();
+            serial.serialize(document);
+
+        }catch(Exception e){
+            System.out.println("Excepción generada");
+            e.printStackTrace();
+        }        
     }
 }
