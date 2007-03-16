cf1a1493a1117fc0195070007646f74a43b9adbb
ramos.re <ramos.re@21717f70-f929-0410-a4ed-59d6e259485b>
2007-03-16 10:50:22 +0000

    primera version de la clase. Aún no vale para nada, pero es necesaria para hacer la clase partida.

---
+++ trunk/ris2k/src/java/model/Turno.java
@@ -0,0 +1,35 @@
+/*
+ * Turno.java
+ *
+ * Created on 15 de marzo de 2007, 11:42
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author Ramon
+ */
+public class Turno {
+    
+    /** Creates a new instance of Turno */
+    public Turno() {
+    }
+    /*******************************************************************/
+    /**                      otros métodos                            **/
+    /*******************************************************************/
+    
+    public boolean TurnoInicial()
+            //TODO: todo, jeje
+    {
+     return true;   
+    }
+    /*******************************************************************/
+    /**                      setter y getter                          **/
+    /*******************************************************************/
+
+    
+}
