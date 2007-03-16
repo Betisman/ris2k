eb9b7329bdf89f958aa2a7b6e72e394135c141cb
ramos.re <ramos.re@21717f70-f929-0410-a4ed-59d6e259485b>
2007-03-16 16:41:23 +0000

    SET UP Y TEAR DOWN de MySQLTest ok, pendientes de nuevas sugerencias Persistir válido, y persistir válido muchos, también OK

--- ris2k/test/persistence/MysqlTest.java
+++ ris2k/test/persistence/MysqlTest.java
@@ -27,10 +27,66 @@ public class MysqlTest extends TestCase {
     }
 
     protected void setUp() throws Exception {
+        
+        System.out.println(">>> setUp");
+        Statement stmt_consul=null, stmt_inser = null; 
+        ResultSet rs = null; 	
+        Connection conn= null;
+        
+        try {
+            try {
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+            } catch (InstantiationException ex) {
+                ex.printStackTrace();
+            } catch (IllegalAccessException ex) {
+                ex.printStackTrace();
+            } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+            }
+          conn =
+          DriverManager.getConnection("jdbc:mysql://localhost/ris2k?user=prueba&password=prueba");          
+        }catch(SQLException ex) {}       
+        try{
+                stmt_inser = conn.createStatement();                 
+                String strSQL = ("DELETE FROM user WHERE user LIKE 'prueba%';");
+                stmt_inser.executeUpdate(strSQL);
+                		
+         } catch (Exception ex) {                
+                System.out.println("NO SE PUDO INICIALIZAR LA BD");
+                throw new Exception("NO SETUP");
+         }
     }
 
     protected void tearDown() throws Exception {
+        System.out.println(">>> tearDown");
+        Statement stmt_consul=null, stmt_inser = null; 
+        ResultSet rs = null; 	
+        Connection conn= null;
+        
+        try {
+            try {
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+            } catch (InstantiationException ex) {
+                ex.printStackTrace();
+            } catch (IllegalAccessException ex) {
+                ex.printStackTrace();
+            } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+            }
+          conn =
+          DriverManager.getConnection("jdbc:mysql://localhost/ris2k?user=prueba&password=prueba");          
+        }catch(SQLException ex) {}       
+        try{
+                stmt_inser = conn.createStatement();                 
+                String strSQL = ("DELETE FROM user WHERE user LIKE 'prueba%';");
+                stmt_inser.executeUpdate(strSQL);
+                		
+         } catch (Exception ex) {                
+                System.out.println("NO SE PUDO HACER EL TEAR DOWN");
+                throw new Exception("NO TEARDOWN");
+         }
     }
+
 /*****************************************************************************/
     /**
      * Tests of persistirJugador method, of class persistence.Mysql.
@@ -41,63 +97,170 @@ public class MysqlTest extends TestCase {
     public void testPersistirJugadorValido() {
         System.out.println(">>> persistirJugadorValido");
         Jugador jugador = new Jugador();
-       
-        System.out.println("BORRAMOS USUARIO DE PRUEBA DE LA BD(Si existe anteriormente)");
-        Mysql.borrarJugador("prueba");
+        GregorianCalendar now = new GregorianCalendar();
         
-        jugador.setUser("prueba");
+        String prueba = String.valueOf(now.getTimeInMillis());
+        jugador.setUser("prueba"+prueba);
         jugador.setPassword("prueba");
         jugador.setEmail("prueba@prueba.com");
-        
-        Mysql.persistirJugador(jugador);
-        
-        boolean expResult = true;
-        boolean result = Mysql.validarJugador(jugador);
-        assertEquals(expResult, result);
-                
-    //    fail("Ha saltado una excepción");
-    }
-    
-    /*prueba que un jugador nulo no se pueda persistir*/
-    public void testPersistirJugadorNulo() throws Exception{
-        System.out.println(">>> persistirJugadorNulo");
+
         try
         {
-            Jugador jugador = null;
+            System.out.println("prueba usuario valida -> insertar prueba"+prueba);
             boolean expResult = true;
             boolean result = Mysql.persistirJugador(jugador);
             assertEquals(expResult, result);
-            fail("Debe lanzarse una excepción");
+            
         }
-        catch (Exception e)
+        catch (Exception ex)
         {
-            //cogemos la excepción para que el caso no falle
+                String mensaje = ex.getMessage();
+                fail("Se ha detectado una excepción: "+ mensaje +"");
         }
+        return;
     }
 
-    /*prueba que un jugador sin nombre no se pueda persistir*/
-    public void testPersistirJugadorSinNombre() throws Exception{
-        System.out.println(">>> persistirJugadorSinNombre");
-        try
+    /*prueba que un numero elevado de jugadores validos se puedan persistir*/
+    public void testPersistirJugadorValidoMuchos() throws Exception{
+        System.out.println(">>> persistirJugadorValidoMuchos");
+        Jugador jugador = new Jugador();
+        String prueba = null;
+        int muchos = 10;
+        int antes,despues;
+        
+        //contamos el número inicial de entradas
+                Statement stmt_consul=null, stmt_inser = null; 
+                ResultSet rs = null; 	
+                Connection conn= null;
+
+                try {
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver").newInstance();
+                    } catch (InstantiationException ex) {
+                        ex.printStackTrace();
+                    } catch (IllegalAccessException ex) {
+                        ex.printStackTrace();
+                    } catch (ClassNotFoundException ex) {
+                        ex.printStackTrace();
+                    }
+                  conn =
+                  DriverManager.getConnection("jdbc:mysql://localhost/ris2k?user=prueba&password=prueba");          
+                }catch(SQLException ex) {}       
+                try{
+                    String sql= "SELECT * from user ";
+                    antes=0;
+                    stmt_consul = conn.createStatement();                
+                    ResultSet rset = stmt_consul.executeQuery(sql);
+                    while (rset.next()) {antes++;}
+
+                 } catch (Exception ex) {                
+                        throw new Exception("NO SE PUDO CONTAR EL NUMERO DE ENTRADAS ANTERIORES");
+                 }
+
+        
+        //ejecutamos el bucle con "muchos" test
+        for(int i=1;i<=muchos;i++)
         {
-            Jugador jugador = null;
-//            jugador.setUser("prueba");
+            GregorianCalendar now = new GregorianCalendar();
+
+            prueba = String.valueOf(now.getTimeInMillis());
+            System.out.println("iteración "+i+" -> insertar prueba"+prueba);
+            jugador.setUser("prueba"+prueba);
             jugador.setPassword("prueba");
             jugador.setEmail("prueba@prueba.com");
+            
+            try 
+            {   
+                boolean expResult = true;
+                boolean result = Mysql.persistirJugador(jugador);
+                assertEquals(expResult, result);
+            }
+            catch (Exception ex) {
+                String mensaje = ex.getMessage();
+                fail("Se ha detectado una excepción: "+ mensaje +"");
+            }
+        }
+        
+        //contamos el número final de entradas
+                try {
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver").newInstance();
+                    } catch (InstantiationException ex) {
+                        ex.printStackTrace();
+                    } catch (IllegalAccessException ex) {
+                        ex.printStackTrace();
+                    } catch (ClassNotFoundException ex) {
+                        ex.printStackTrace();
+                    }
+                  conn =
+                  DriverManager.getConnection("jdbc:mysql://localhost/ris2k?user=prueba&password=prueba");          
+                }catch(SQLException ex) {}       
+                try{
+                    String sql= "SELECT * from user ";
+                    despues=0;
+                    stmt_consul = conn.createStatement();                
+                    ResultSet rset = stmt_consul.executeQuery(sql);
+                    while (rset.next()) {despues++;}
 
-            boolean expResult = true;
-            boolean result = Mysql.persistirJugador(jugador);
-            assertEquals(expResult, result);
-            fail("Debe lanzarse una excepción");
-        }
-        catch (Exception e)
-        {
-            //cogemos la excepción para que el caso no falle
-        }
+                 } catch (Exception ex) {                
+                        throw new Exception("NO SE PUDO CONTAR EL NUMERO DE ENTRADAS POSTERIORES");
+                 }
+
+        //comparamos si el número de entradas introducido coincide realmente con "muchos"
+        
+        if (despues-antes==muchos) return;
+        else throw new Exception("NO SE INSERTARON TODOS");
     }
+    
+    /*prueba que un jugados nulo no se pueda persistir*/
+    public void testPersistirJugadorNulo() throws Exception{
+        System.out.println(">>> persistirJugadorNulo");
+        Jugador jugador = new Jugador();
+ 
+        
+        try
+        {
+            Mysql.persistirJugador(jugador);
+        }
+  
+        catch (Exception ex)
+        {
+            String expResult = "NO INSERT";
+            String result = ex.getMessage();
+            assertEquals(expResult, result);
+            return;
+        }
+        fail("Se esperaba una excepción NO INSERT ");
+        
+    }        
 
+    /*prueba que un jugador sin nombre no se pueda persistir*/
+    /*public void testPersistirJugadorSinNombre() throws Exception{
+        System.out.println(">>> persistirJugadorSinNombre");
+        Jugador jugador = new Jugador();
+//        jugador.setUser("prueba");
+        jugador.setPassword("prueba");
+        jugador.setEmail("prueba@prueba.com");
+ 
+        try
+        {
+            Mysql.persistirJugador(jugador);
+        }
+  
+        catch (Exception ex)
+        {
+
+            String expResult = "NO INSERT";
+            String result = ex.getMessage();
+            assertEquals(expResult, result);
+            return;
+        }
+        fail("Debe lanzarse una excepción NO INSERT");
+        
+    }
+*/
     /*prueba que un jugador sin password no se pueda persistir*/
-    public void testPersistirJugadorSinPassword() throws Exception{
+  /*  public void testPersistirJugadorSinPassword() throws Exception{
         System.out.println(">>> persistirJugadorSinPassword");
         try
         {
@@ -116,9 +279,9 @@ public class MysqlTest extends TestCase {
             //cogemos la excepción para que el caso no falle
         }
     }
-    
+    */
     /*prueba que un jugador sin email no se pueda persistir*/
-    public void testPersistirJugadorSinEmail() throws Exception{
+   /* public void testPersistirJugadorSinEmail() throws Exception{
         System.out.println(">>> persistirJugadorSinEmail");
         try
         {
@@ -137,7 +300,7 @@ public class MysqlTest extends TestCase {
             //cogemos la excepción para que el caso no falle
         }
     }
-    
+    */
 /*****************************************************************************/
     /**
      * Tests of borrarJugador method, of class persistence.Mysql.
@@ -145,7 +308,7 @@ public class MysqlTest extends TestCase {
 /*****************************************************************************/
     
    /*prueba si un jugador existente se puede borrar*/
-    public void testBorrarJugador() {
+   /* public void testBorrarJugador() {
         System.out.println(">>> borrarJugadorExistente");
         Jugador jugador = new Jugador();
        
@@ -154,7 +317,11 @@ public class MysqlTest extends TestCase {
         jugador.setEmail("prueba@prueba.com");
         
         System.out.println("INSERTAMOS USUARIO DE PRUEBA DE LA BD(Si no existe anteriormente)");
-        Mysql.persistirJugador(jugador);
+        try {
+            Mysql.persistirJugador(jugador);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         
         
         Mysql.borrarJugador("prueba");
@@ -165,7 +332,7 @@ public class MysqlTest extends TestCase {
                 
     //    fail("Ha saltado una excepción");
     }
-
+*/
 
 /*****************************************************************************/
     /**
@@ -173,7 +340,7 @@ public class MysqlTest extends TestCase {
      */
 /*****************************************************************************/
 
-    public void testValidarJugadorValido() {
+  /*  public void testValidarJugadorValido() {
         System.out.println(">>> ValidarJugadorValido");
  
         Jugador jugador = new Jugador();
@@ -185,7 +352,11 @@ public class MysqlTest extends TestCase {
         jugador.setEmail("prueba@prueba.com");
         
         Mysql.borrarJugador("prueba");
-        Mysql.persistirJugador(jugador);
+        try {
+            Mysql.persistirJugador(jugador);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         
  //       TODO:                
         
@@ -233,7 +404,11 @@ public class MysqlTest extends TestCase {
         jugador.setEmail("prueba@prueba.com");
         
         Mysql.borrarJugador("prueba");
-        Mysql.persistirJugador(jugador);
+        try {
+            Mysql.persistirJugador(jugador);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         
         try
         {
@@ -250,6 +425,6 @@ public class MysqlTest extends TestCase {
             //cogemos la excepción para que el caso no falle
         }
     }
-    
+    */
     //*************************************************************************************
 }
