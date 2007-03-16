4c78166a054856f43d2767283725559e42b8cb46
ramos.re <ramos.re@21717f70-f929-0410-a4ed-59d6e259485b>
2007-03-16 10:48:30 +0000

    primera version de la clase. Aún no vale para nada, pero espero que os sirva para tener una idea...

---
+++ ris2k/src/java/model/Partida.java
@@ -0,0 +1,122 @@
+/*
+ * Partida.java
+ *
+ * Created on 15 de marzo de 2007, 10:58
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package model;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author Ramon
+ */
+public class Partida {
+    private String nombre = null; 
+    
+    private Jugador owner = null;
+    private Tablero tablero = null;
+    
+    private int numJugadores = 0;
+    private Vector <Jugador> jugadores= null;
+    
+    private Turno turno = new Turno();
+    
+    private Score score = new Score();
+    
+
+    
+    
+    /** Creates a new instance of Partida */
+    public Partida() {
+        
+    }
+    /*******************************************************************/
+    /**                      otros métodos                            **/
+    /*******************************************************************/
+    
+    public boolean inicializar(String nombre, Jugador owner, Tablero tablero, int maxjugadores)
+    throws Exception
+    {
+        try{
+        this.setNombre(nombre);
+        this.setOwner(owner);
+        this.setTablero(tablero);
+        this.setNumJugadores(maxjugadores);
+        
+        
+        turno.TurnoInicial();
+        return true;
+        }
+        catch (Exception ex)
+        {
+            System.out.println("El método ha fallado");
+        }
+        return false;
+    }
+    /*******************************************************************/
+    /**                      setter y getter                          **/
+    /*******************************************************************/
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Jugador getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Jugador owner) {
+        this.owner = owner;
+    }
+
+    public Tablero getTablero() {
+        return tablero;
+    }
+
+    public void setTablero(Tablero tablero) {
+        this.tablero = tablero;
+    }
+
+    public int getNumJugadores() {
+        return numJugadores;
+    }
+
+    public void setNumJugadores(int numJugadores) {
+        this.numJugadores = numJugadores;
+    }
+
+    public Vector<Jugador> getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(Vector<Jugador> jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    public Turno getTurno() {
+        return turno;
+    }
+
+    public void setTurno(Turno turno) {
+        this.turno = turno;
+    }
+
+    public Score getScore() {
+        return score;
+    }
+
+    public void setScore(Score score) {
+        this.score = score;
+    }
+    
+    
+}
