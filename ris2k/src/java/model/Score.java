92417925802df17c8818d3acd8c8857773547dee
ramos.re <ramos.re@21717f70-f929-0410-a4ed-59d6e259485b>
2007-03-16 10:50:12 +0000

    primera version de la clase. Aún no vale para nada, pero es necesaria para hacer la clase partida.

---
+++ ris2k/src/java/model/Score.java
@@ -0,0 +1,34 @@
+/*
+ * Score.java
+ *
+ * Created on 15 de marzo de 2007, 11:47
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author Ramon
+ */
+public class Score {
+    
+    /** Creates a new instance of Score */
+    public Score() {
+    }
+    /*******************************************************************/
+    /**                      otros métodos                            **/
+    /*******************************************************************/
+    
+    public boolean ScoreInicial()
+            //TODO: todo, jeje
+    {
+     return true;   
+    }
+    /*******************************************************************/
+    /**                      setter y getter                          **/
+    /*******************************************************************/
+    
+}
