c8d1140d6149567384f7b5ef0f5af2a7f1cddc60
ramos.re <ramos.re@21717f70-f929-0410-a4ed-59d6e259485b>
2007-03-22 17:55:29 +0000

    se borran pruebas chorras, se sube web xml para depurar problemas errorform

--- trunk/ris2k/src/java/controller/accesoServlet.java
+++ trunk/ris2k/src/java/controller/accesoServlet.java
@@ -23,69 +23,29 @@ import model.Jugador;
 public class accesoServlet extends MiServlet {   
     String error1=null; //Defino este string para saber que tipo de error de validación se produce.
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+    throws ServletException, IOException 
+    {
         Jugador jugador = new Jugador(); 
         String user = request.getParameter("user");
         String password = request.getParameter("password");
-        /*int contador = 0;
-        String errorBD = null;
-        String errorUser1 = null;
-        String errorUser2 = null;
-        String Password1 = null;
-        String Password2 = null;
-        
-        String user = request.getParameter("user");       
-        request.getSession().setAttribute("errorUser1","");
-        if(user.length()>128 || !user.matches("[a-zA-Z0-9]+")){ 
-         contador++;
-         request.getSession().setAttribute("errorUser1","¡ERROR! Nombre de usuario incorrecto, sólo puede contener letras y dígitos");
-         System.out.println("FALLO: "+user);            
+        jugador.setUser(user);
+        jugador.setPassword(password);
+        try {
+            Mysql.validarJugador(jugador);
+            request.getSession().setAttribute("jugador","Bienvenid@, "+user.toUpperCase());
+            /*Al ser un jugador válido, lo metemos en el objeto session*/
+            request.getSession().setAttribute("usuario", jugador);
+                /***********************************************************/
+            gotoJSPPage(menuForm,request,response);
+
+        } catch (ris2kException ex) {
+            request.getSession().setAttribute("errorRis2k",ex.getMessage());
+            gotoJSPPage(errorForm,request,response);
         }
-        
-        request.getSession().setAttribute("errorUser2","");
-        if(user.matches("puta")||user.matches("cabron[a-zA-Z]*")||user.matches("mierda")){ 
-         contador++;
-         request.getSession().setAttribute("errorUser2","¡ERROR! Ese nombre de usuario puede resultar ofensivo, no se pudo registrar con él");
-         System.out.println("FALLO: "+user);            
-        }
-        
-        
-        String password = request.getParameter("password");
-    
-        request.getSession().setAttribute("errorPassword1","");
-        if (password.length()<6){
-            contador++;
-            request.getSession().setAttribute("errorPassword1","¡ERROR! La contraseña es demasiado corta (mínimo 6 caracteres)");
-            System.out.println("FALLO: "+password);
-        }
-        
-        request.getSession().setAttribute("errorPassword2","");
-        if (!password.matches("[a-zA-Z0-9]+")){
-            contador++;
-            request.getSession().setAttribute("errorPassword2","¡ERROR! La contraseña sólo puede contener letras y dígitos");
-            System.out.println("FALLO: "+password);
-        }
-       
-        request.getSession().setAttribute("errorBD","");
-        if (contador == 0){*/
-            jugador.setUser(user);
-            jugador.setPassword(password);
-            try {
-                Mysql.validarJugador(jugador);
-                request.getSession().setAttribute("jugador","Bienvenid@, "+user.toUpperCase());
-                /*Al ser un jugador válido, lo metemos en el objeto session*/
-                request.getSession().setAttribute("usuario", jugador);
-                    /***********************************************************/
-                 gotoJSPPage(menuForm,request,response);
-                
-            } catch (ris2kException ex) {
-                request.getSession().setAttribute("errorRis2k",ex.getMessage());
-                gotoJSPPage(errorForm,request,response);
-            }
-           request.getSession().setAttribute("errorRis2k","Error Desconocido");
-           gotoJSPPage(errorForm,request,response);
-            
-        
+       request.getSession().setAttribute("errorRis2k","Error Desconocido");
+       gotoJSPPage(errorForm,request,response);
+
+
     }  
   
     /** Handles the HTTP <code>GET</code> method.
--- trunk/ris2k/web/view/errorForm.jsp
+++ trunk/ris2k/web/view/errorForm.jsp
@@ -12,6 +12,7 @@ on Libraries node in Projects view can be used to add the JSTL 1.1 library.
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
    "http://www.w3.org/TR/html4/loose.dtd">
 
+  
 <html>
     <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
