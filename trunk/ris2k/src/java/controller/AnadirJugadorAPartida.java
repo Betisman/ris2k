81b0efd74e358d82cdd65e4bb17c6e05d6fd900d
msfer2000 <msfer2000@21717f70-f929-0410-a4ed-59d6e259485b>
2007-04-23 10:07:01 +0000

    Añadidos log en esta clase.

--- trunk/ris2k/src/java/controller/AnadirJugadorAPartida.java
+++ trunk/ris2k/src/java/controller/AnadirJugadorAPartida.java
@@ -10,12 +10,13 @@ import Exceptions.ris2kException;
 import java.io.*;
 import java.net.*;
 import java.util.List;
-
 import javax.servlet.*;
 import javax.servlet.http.*;
 import model.Jugador;
 import model.Partida;
 import persistence.MySqlPartida;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 /**
  *
@@ -23,7 +24,7 @@ import persistence.MySqlPartida;
  * @version
  */
 public class AnadirJugadorAPartida extends MiServlet {
-    
+    static Logger log = Logger.getLogger(AnadirJugadorAPartida.class);
     /** Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
@@ -33,15 +34,18 @@ public class AnadirJugadorAPartida extends MiServlet {
         //recuperamos el id de la partida que va implícito en el request de la petición http
         String idPartida = request.getParameter("partida");
         try {
-            System.out.println("anadirJugadorAPartida");
+            //System.out.println("anadirJugadorAPartida");
+            log.info("anadirJugadorAPartida");
             Jugador jugadorActual = (Jugador)request.getSession().getAttribute("usuario");
-            System.out.println(jugadorActual.getUser() + " (" + jugadorActual.getPassword() + ") - " + jugadorActual.getEmail());
+            //System.out.println(jugadorActual.getUser() + " (" + jugadorActual.getPassword() + ") - " + jugadorActual.getEmail());
+            log.info(jugadorActual.getUser() + " (" + jugadorActual.getPassword() + ") - " + jugadorActual.getEmail());
             Partida partida = MySqlPartida.getPartida(idPartida);
             if (partida.estaJugador(jugadorActual)){
                 System.out.println("La partida " + partida.getNombre() + " ya contiene al jugador " + jugadorActual.getUser());
             }else{
                 partida.getJugadores().add(jugadorActual);
-                System.out.println("Añadimos a la partida " + partida.getNombre() + " el jugador " + jugadorActual.getUser());
+                //System.out.println("Añadimos a la partida " + partida.getNombre() + " el jugador " + jugadorActual.getUser());
+                log.info("Añadimos a la partida " + partida.getNombre() + " el jugador " + jugadorActual.getUser());
                 MySqlPartida.persistirPartida(partida);
             }                
             
